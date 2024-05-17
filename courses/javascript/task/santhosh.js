77aa0fc5cb0e59b34a4e3f719ebaee02d4b95c34
Santhosh Nimmala <111045464+santhosh799@users.noreply.github.com>
2024-05-17 13:23:43 +0000

    santhosh details added in js file with the help of variables

---
+++ courses/javascript/task/santhosh.js
@@ -0,0 +1,7 @@
+var name = "Santhosh Nimmala"
+var email = "santhoshnimmala799@gmail.com"
+var location = "Suryapet, Telangana"
+var Designation = "Software Engineer"
+var company = "Zakboys"
+var Skills = " HTML, CSS, Javascript, ReactJs, Redux For State Management and for styling using Bootstrap and tailwind Css"
+
