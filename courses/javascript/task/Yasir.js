3be916eb279cbb7b7ec1e5eef6598e2e35fc90d8
M Yasir <110448049+mdyasir1@users.noreply.github.com>
2024-05-20 02:01:53 +0000

    Yasir's js file added

---
+++ courses/javascript/task/Yasir.js
@@ -0,0 +1,11 @@
+var firstname="Yasir"
+var lastname="Mohammed"
+
+var fullname=firstname+lastname
+
+var location="Vijayawada"
+var email="mdyasir4145@gmail.com"
+
+console.log(fullname)
+console.log(location)
+console.log(email)
