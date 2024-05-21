7817a4bc7f72ad9f050405d7d5d2333956d6061a
Rahamansir <153214534+Rahamansir@users.noreply.github.com>
2024-05-21 12:28:29 +0000

    ``

--- courses/javascript/task/Rahaman.js
+++ courses/javascript/task/Rahaman.js
@@ -1,7 +1,9 @@
 var first = "Shaik";
 var middlename = "Abdul";
 var LastName = "Rahaman";
+var Skills = ["HTML","CSS","JS"]
+var designation = "zsm"
+var area = "Guntur"
 
-var Skills = "HTML","CSS","JS";
-
-console.log(first,middlename,LastName)
+console.log(first,middlename,LastName,)
+console.log(`my name is ${first} ${middlename} ${LastName} i am from ${area} My Skills Are ${Skills} My designation is ${designation}`)
