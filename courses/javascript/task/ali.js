0ec7d30f2e873e2755e75ddb3aaf13cf54cc838b
mohammedAli46 <94938119+mohammedAli46@users.noreply.github.com>
2024-05-21 11:23:42 +0000

    ali_JS file Updated

---
+++ courses/javascript/task/ali.js
@@ -0,0 +1,10 @@
+var firstName = "Shaik Mohammad";
+var middleName = "Mohammad";
+var lastName = "Ali";
+
+var fullName = firstname + " " + middleName + " " + lastname;
+var basicSkills = "HTML, CSS, JS";
+var address_location = "Hyderabad";
+
+console.log(fullName + "from" + address_location);
+console.log(skills);
