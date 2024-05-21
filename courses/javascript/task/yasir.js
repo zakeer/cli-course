20197f84c6f4c46d4f1fd8192556c61ed03cf006
M Yasir <110448049+mdyasir1@users.noreply.github.com>
2024-05-21 12:45:18 +0000

    updated .js

--- courses/javascript/task/yasir.js
+++ courses/javascript/task/yasir.js
@@ -2,9 +2,9 @@ var firstname = "Yasir";
 var lastname = " Mohammed";
 
 var fullname =firstname + lastname;
- 
+
 var location= " Vijayawada";
 var email="mdyasir4145@gmail.com";
 
-console.log(`${fullname} from ${location}`)
+console.log(`${fullname} from${location}`)
 console.log(`My email is ${email}`)
