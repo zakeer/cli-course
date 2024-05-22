9502a72bc01009b4c47fea50cd1fb753904ca664
Rahamansir <153214534+Rahamansir@users.noreply.github.com>
2024-05-22 11:30:35 +0000

    Changes

--- courses/javascript/task/Rahaman.js
+++ courses/javascript/task/Rahaman.js
@@ -1,9 +1,9 @@
 var first = "Shaik";
 var middlename = "Abdul";
-var LastName = "Rahaman";
-var Skills = ["HTML","CSS","JS"]
-var designation = "zsm"
-var area = "Guntur"
+var lastName = "Rahaman";
+var skills = ["HTML","CSS","JS"];
+var designation = "zsm";
+var area = "Guntur";
 
 console.log(first,middlename,LastName,)
 console.log(`my name is ${first} ${middlename} ${LastName} i am from ${area} My Skills Are ${Skills} My designation is ${designation}`)
