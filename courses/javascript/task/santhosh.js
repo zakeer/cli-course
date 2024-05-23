ea328f6255b0c7e81edb3d4e964848a375e5cece
Santhosh Nimmala <111045464+santhosh799@users.noreply.github.com>
2024-05-23 10:31:35 +0000

    Salery Details Added

--- courses/javascript/task/santhosh.js
+++ courses/javascript/task/santhosh.js
@@ -10,3 +10,29 @@ var salary = 48000;
 var output = `${firstname} ${lastname} living near ${address} and working has ${designation} for ${company} and earning around ${salary * 12} per year`
 
 console.log(output);
+
+//Salary Calculations
+var basicSalery = 660000;
+
+var pfContributionRatePercentage = 0.12;   //    12/100
+
+var otherAllowancesPercentage = 0.20;    //   20/100
+
+var pfContributionOfEmployee = basicSalery * pfContributionRatePercentage; 
+
+var pfContributionOfEmployer = basicSalery * pfContributionRatePercentage;
+
+var otherAllowances = basicSalery * otherAllowancesPercentage;
+
+var grossSalery = basicSalery + otherAllowances;
+
+var totalDeductions = pfContributionOfEmployee + pfContributionOfEmployer;
+
+var takeHomeSalery = grossSalery - totalDeductions;
+
+console.log(takeHomeSalery);
+
+
+
+
+
