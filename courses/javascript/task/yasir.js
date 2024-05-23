30a3d943ac8e373dc24bea12a773fe3e372a7434
M Yasir <110448049+mdyasir1@users.noreply.github.com>
2024-05-23 09:32:08 +0000

    arithmetic and comparision operators added

--- courses/javascript/task/yasir.js
+++ courses/javascript/task/yasir.js
@@ -8,3 +8,17 @@ var email="mdyasir4145@gmail.com";
 
 console.log(`${fullname} from ${location}`)
 console.log(`My email is ${email}`)
+
+// Arithmetic operators
+x=6;
+y=2;
+console.log (`The addition of two variables is ${x+y}`) // Addition
+console.log (`The subtraction of two variables is ${x-y}`) // Subtraction
+console.log (`The multiplication of two variables is ${x*y}`) // Multiplication
+console.log (`The division of two variables is ${x/y}`) // Division
+console.log (`The modulus of two variables is ${x%y}`) // Modulus
+
+// Comparision operators
+typeof 10==10
+typeof "yasir"=="yasir" 
+//It will be false. because, typeof"yasir" is string on the left side. on the right side it is "yasir". so, it is false.
