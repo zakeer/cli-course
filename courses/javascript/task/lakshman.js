73370e3a40ebb4f8107387d961d3727ef42bec0d
MAKKA LAKSHMAN <168202169+makka9392@users.noreply.github.com>
2024-06-22 06:16:32 +0000

    courses/javascript/task/lakshman.js

--- courses/javascript/task/lakshman.js
+++ courses/javascript/task/lakshman.js
@@ -3,3 +3,12 @@ var lastname = "lakshman";
 var fullname = firstname + lastname;
 var skills = "HTML, CSS, JS";
 var address = "guntur";
+
+
+
+// console.log(fullname + " " + "from" + " " + address);
+// console.log(fullname + " from " + address + " and my skills are: " + skills);
+// `literal value ${identifier}`
+console.log(`${fullname} from ${address} and my skills are ${skills}`)
+
+console.log();
